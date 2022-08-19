b478299e9baa0f785cd0dc4bdd68afb6a79182da
allen dean <allen7242@gmail.com>
2022-08-18 20:27:59 -0500

    first commit

--- indexed.js
+++ indexed.js
@@ -1,12 +1,47 @@
 import { openDB } from "idb";
-export const database = openDB("movieStore", 2, {
+const apiKey = import.meta.env.VITE_API_KEY;
+
+export const database = openDB("MovieStore", 2, {
   upgrade(db) {
-    db.createObjectStore("MoviesToStore", {
+    db.createObjectStore("moviesToStore", {
       keyPath: "id",
+
+      autoIncrement: true,
+    });
+
+    db.createObjectStore("FavmoviesToStore", {
+      keyPath: "fav",
+
       autoIncrement: true,
     });
   },
 });
+
+// import { openDB } from 'idb';
+// const apiKey = import.meta.env.VITE_API_KEY;
+
+// export const database =  openDB('MovieStore', 2, {
+//     upgrade(db) {
+//      db.createObjectStore('moviesToStore',    {keyPath:"id",
+
+//        autoIncrement: true });
+
+//        db.createObjectStore('FavmoviesToStore',    {keyPath:"fav",
+
+//        autoIncrement: true });
+
+//     },
+//   });
+
+// import { openDB } from "idb";
+// export const database = openDB("movieStore", 2, {
+//   upgrade(db) {
+//     db.createObjectStore("MoviesToStore", {
+//       keyPath: "id",
+//       autoIncrement: true,
+//     });
+//   },
+// });
 
 //function
 // a function is const myfunc = () => {
@@ -18,3 +53,22 @@ export const database = openDB("movieStore", 2, {
 
 //}
 
+// //opening the data base
+// import { openDB } from 'idb';
+
+// export const database = openDB('movieStore', 2, {
+
+// upgrade(db) {
+// db.createObjectStore("MoviesToStore", { keyPath: "id" }, { autoIncrement: true });
+//  },
+//  });
+
+// //function
+// // a function is const myfunc = () => {
+
+// //}
+// // method
+// // a method is myMethod() {
+// // console.log("");
+
+// //}
